fc972e8be188d71e728a70450b99aaae3b47435e
Isabella98Liu <isabellaliu98@berkeley.edu>
2018-03-03 13:36:48 +0800

    Update1.1

---
+++ SteamVR_ToolKit_1.1/ignoreCollision.cs
@@ -0,0 +1,32 @@
+﻿//******************************************************************************************************
+// Latest Update : 3/3/2018
+// Author : Isabella Liu
+// this script should be attached to SteamVR [CameraRig]
+// Function : ignore the collision between CameraRig rigidbody and dragable object's regidobdy when user using controller dragging objects
+// SDK Requirement : [SteamVR Plugin] https://assetstore.unity.com/packages/templates/systems/steamvr-plugin-32647
+// Notification : None
+//*******************************************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ignoreCollision : MonoBehaviour {
+
+    private GameObject cameraEye;
+
+    private void Start()
+    {
+        cameraEye = GameObject.Find("/[CameraRig]/Camera (eye)");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+//            Debug.Log("Cube collider entered CameraRig, ignoreCollision enable.");
+            Physics.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider>());
+            Physics.IgnoreCollision(collision.collider, cameraEye.GetComponent<Collider>());
+        }
+    }
+}
---
+++ SteamVR_ToolKit_1.1/wearGoggles.cs
@@ -0,0 +1,33 @@
+﻿//******************************************************************************************************
+// Latest Update : 3/3/2018
+// Author : Isabella Liu
+// this script should be attached to [Frame] under [SafetyGlasses]
+// Function : adding visual effect after wear goggles  (when the distance between goggles and CameraEye is small enough)
+// SDK Requirement : [SteamVR Plugin] https://assetstore.unity.com/packages/templates/systems/steamvr-plugin-32647
+// Notification : None
+//*******************************************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wearGoggles : MonoBehaviour {
+
+    private GameObject cameraEye;
+
+	// Use this for initialization
+	void Start () {
+        cameraEye = GameObject.Find("/[CameraRig]/Camera (eye)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(gameObject.transform.position, cameraEye.transform.position);
+        //       Debug.Log("The distance between Eye and Goggles are : " + distance);
+        if (distance < 0.3f)
+        {
+            Debug.Log("You have wear your safty goggles.");
+        }
+	}
+
+}
